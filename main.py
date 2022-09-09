bbe23e7f601262a2d2871a9d0e8e25767b333171
schoolevdev <loddere14207@stxavier.org>
2022-09-09 14:03:37 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# Lab02avst.py
+# This is the Student, Starting file of Lab02a.
+# The Quarter, Dime & Nickel Program
+#
+def main() -> None:
+    print(23)
+
+
+if __name__ == "__main__":
+    main()
