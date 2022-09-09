3632a162addaf975ebb449a38bfb3c856bdefae8
bobobo34 <81543088+bobobo34@users.noreply.github.com>
2022-09-09 14:33:25 -0400

    complete in like 2 minutes

--- main.py
+++ main.py
@@ -3,7 +3,11 @@
 # The Quarter, Dime & Nickel Program
 #
 def main() -> None:
-    print(23)
+    for quarters in range(3): # 0 - 2 quarters (2 is max)
+        for dimes in range(6): # 0 - 5 dimes
+            for nickels in range(11): # 0 - 10 nickels
+                if quarters * 25 + dimes * 10 + nickels * 5 == 50:
+                    print(f"{quarters} Quarters {dimes} dimes {nickels} nickels == 50 cents")
 
 
 if __name__ == "__main__":
