a810e14a8747d7f3d578722bc5ab6df60e03b2df
schoolevdev <loddere14207@stxavier.org>
2022-09-15 13:25:40 -0400

    added input functtionality

--- main.py
+++ main.py
@@ -2,12 +2,16 @@
 # This is the Student, Starting file of Lab02a.
 # The Quarter, Dime & Nickel Program
 #
+from math import floor
+
+
 def main() -> None:
-    for quarters in range(3): # 0 - 2 quarters (2 is max)
-        for dimes in range(6): # 0 - 5 dimes
-            for nickels in range(11): # 0 - 10 nickels
-                if quarters * 25 + dimes * 10 + nickels * 5 == 50:
-                    print(f"{quarters} Quarters {dimes} dimes {nickels} nickels == 50 cents")
+    cents: int = eval(input("Enter # of cents:"))
+    for quarters in range(floor(cents / 25) + 1): # 0 - 2 quarters (2 is max)
+        for dimes in range(floor(cents / 10) + 1): # 0 - 5 dimes
+            for nickels in range(floor(cents / 5) + 1): # 0 - 10 nickels
+                if quarters * 25 + dimes * 10 + nickels * 5 == cents:
+                    print(f"{quarters} Quarters {dimes} dimes {nickels} nickels == {cents} cents")
 
 
 if __name__ == "__main__":
